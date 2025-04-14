2336d78a0c85b6775d2672a3e45345c6449341cf
Ananya_Majety <137465897+Ananya-1227@users.noreply.github.com>
2025-04-14 17:50:39 +0530

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -7,10 +7,16 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))  # Ensure this key is set in your environment
 
-def gemini_answer(prompt: str) -> str:
+def gemini_answer(prompt: str,max_tokens=100) -> str:
     try:
         model = genai.GenerativeModel("gemini-1.5-pro-latest")  # Use the correct model name
-        response = model.generate_content(prompt,generation_config={"temperature":0.4})
+        response = model.generate_content(prompt,generation_config=genai.types.GenerationConfig(
+            max_output_tokens=max_tokens,
+            temperature=0.3,
+            top_p=0.7,
+            stop_sequences=["\n\n"]
+            )
+        )
         clean_text=re.sub(r'<[^>]+>','',response.text)
         clean_text = re.sub(r'[\*\_]', '', clean_text)  # Remove * and stuff
         return clean_text.strip()
