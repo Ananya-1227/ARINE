1f7148c4fa4d909287249b3f7f0d7c7df69b5152
Ananya_Majety <137465897+Ananya-1227@users.noreply.github.com>
2025-04-21 17:50:03 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_oauth import OAuth2Component
-# from my_search import search_and_respond
+from my_search import get_answer_from_deepseek
 from deepseek import deepseek_answer
 from db import (
     init_db,
